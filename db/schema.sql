49f14a8db8406c501aa7cb1220fb5e4325c36085
steepllama <steepllama@gmail.com>
2022-02-04 23:31:27 -0600

    added the initial files, tables on the schema file, and the dependecies on the package json

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE department_table(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role_table(
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2),
+    department_id INT
+    PRIMARY KEY(id)
+
+    CONSTRAINT fk_department_table
+    FOREIGN KEY (department_id)
+    REFERENCES department_table(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employee_table(
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT 
+)
