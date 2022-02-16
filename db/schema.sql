4d2b1b6e80325fbfeda8a9b4cebe970656a8389a
steepllama <steepllama@gmail.com>
2022-02-16 08:17:31 -0600

    updated the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,18 @@
--- DROP DATABASE IF EXISTS subordinates_table;
-CREATE DATABASE department_table;
+CREATE DATABASE IF EXISTS department_table;
+CREATE DATABASE department_db;
+USE department_db;
 
-USE department_table;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roleTable;
+DROP TABLE IF EXISTS employeeTable;
 
-CREATE TABLE department_table(
+CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
 
-CREATE TABLE role_table(
+CREATE TABLE roleTable(
     id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2),
@@ -18,18 +21,18 @@ CREATE TABLE role_table(
 
     CONSTRAINT fk_department_table
     FOREIGN KEY (department_id)
-    REFERENCES department_table(id)
+    REFERENCES department(id)
     ON DELETE CASCADE
 );
 
-CREATE TABLE employee_table(
-    id INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE employeeTable(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT 
 
     CONSTRAINT fk_department_table
     FOREIGN KEY (department_id)
-    REFERENCES department_table(id)
+    REFERENCES department(id)
     ON DELETE CASCADE
 );
